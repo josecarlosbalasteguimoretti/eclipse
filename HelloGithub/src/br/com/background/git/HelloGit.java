41a12a22c2c9259247842eecf7c54000c9f4879d
José Carlos Balastegui Moretti <62955995+josecarlosbalasteguimoretti@users.noreply.github.com>
2020-06-23 16:48:35 -0300

    adicionei arquivos do eclipse workspace via upload

---
+++ HelloGithub/src/br/com/background/git/HelloGit.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package br.com.background.git;
+
+/**
+ * @author ZeCaMo
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Github");
+		
+		
+		
+
+	}
+
+}
---
+++ HelloGithub/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ZeCaMo
+ *
+ */
+module helloGithub {
+}
